9cb4870b1285b89f5e325511c29c139a1b35af5d
leeyeah <19901108sr@sina.com>
2022-08-04 14:42:21 +0800

    Delete traj_management directory

--- traj_management/traj_chunk_read/example/aisread.py
+++
@@ -1,5 +0,0 @@
-from pyais import chunk_read
-
-if __name__ == '__main__':
-    # chunk_read.read_ais_data_region(input="../data/1.csv", output='../data/test.csv')  # 此处填写csv路径
-    chunk_read.read_ais_data_mmsi(input="../data/1.csv", output='../data/test.csv', mmsi='244726000')  # 此处填写csv路径
